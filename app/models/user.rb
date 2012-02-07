6c6b56a2154c84297b9a932d0084a0c674839452
Darren Royle <darren.royle@oup.com>
2012-02-07 17:36:18 +0000

    Time to go home, done some work on Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,20 @@ class User < ActiveRecord::Base
   
   private
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
     
     def encrypt(string)
-      string #Replace with actual encryption!
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt 
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string) 
+      Digest::SHA2.hexdigest(string)
     end
 end
 
