90abd0b8bd7ee66e90ce8d9d3adf3a53972ab7b4
Darren Royle <darren.royle@oup.com>
2012-07-06 16:11:27 +0100

    Updating images etc

--- db/migrate/20120207162616_create_users.rb
+++ db/migrate/20120207162616_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
       t.string :name
       t.string :email
 
       t.timestamps
     end
+  end
+  def down
+    drop_table :users
   end
 end
