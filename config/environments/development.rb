abbb3c4439153e75a54b135ae7966dc23b2c1f1d
Darren Royle <darren.royle@oup.com>
2012-07-09 13:15:13 +0100

    Imagemagick variables incorrect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ MasterBaker::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  Paperclip.options[:command_path] = "/opt/local/bin/convert"
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
